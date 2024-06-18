3486de95760c8f5a5b90a7e9e3cb0747752c1924
Shivasai Badam <116059026+shivasaibadam22@users.noreply.github.com>
2024-06-18 16:20:42 +0530

    Delete Basics directory

--- Basics/demo1.js
+++
@@ -1,9 +0,0 @@
-function geekFunc() {
-    try {
-        console.log("Value of variable a is : " + a);
-    }
-    catch (e) {
-        console.log("Error: " + e.description);
-    }
-}
-geekFunc();
